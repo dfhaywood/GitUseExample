908a0762f92b7a4349f4e02573a981cc164a5580
David Haywood <david@dfhaywood.com>
2018-03-05 16:42:27 -0600

    Added Map Feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by David Haywood on 3/5/18.
+//  Copyright © 2018 dfhaywood. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
