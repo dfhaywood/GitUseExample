bb3142b80db186ab1bfffb60be47ec657ecb4514
David Haywood <david@dfhaywood.com>
2018-03-05 20:50:31 -0600

    Final change to "buttonClicked"

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonClicked(_sender: UIButton) {
-        print("This is a Git tutorial")
+        print("You Finished!")
     }
 
 
